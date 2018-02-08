e76adef9326e9a84ab8c353cdec050c017c4fbc8
Alan <alan@salsadigital.com.au>
2018-02-08 12:25:16 +1100

    2.12 - Remove additional jsbeautify install and locked jsbeautify package to 1.6.8

--- updates.sh
+++
@@ -1,5 +0,0 @@
-# Installs the old grunt-jsbeautifier version.
-rm -rf node_modules/grunt-jsbeautifier
-tar -xzf ./grunt-jsbeautifier.zip
-cp -R grunt-jsbeautifier node_modules/grunt-jsbeautifier
-rm -rf grunt-jsbeautifier
